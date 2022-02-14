7590274a61ec3600eb0dfc6ef0bb9a86898d59c0
Houjun Liu <hliu@shabang.cf>
2022-02-14 11:01:52 -0800

    unspported const generic wait

--- src/main.rs
+++ src/main.rs
@@ -69,8 +69,68 @@ fn insertion_sort(list:&[u32]) -> Vec<u32> {
     return vec;
 }
 
-// Worse and best case insertion: do nothing + do everything
+/// Counting sort
+///
+/// # Args
+/// - list:`&[u32]` - slice of list to be sorted
+/// - max:`usize` - the maximum value possible to be passed in
+///
+/// # Returns
+/// `Vec<u32>` representing the sorted list
+fn counting_sort(list:&[u32], max:usize) -> Vec<u32> {
+    // We first create the buckets from which we sort
+    // let mut buckets = [0usize; max];
 
+    // We then create that many buckets. 
+
+
+    // We first clone the slice into a vector which we will
+    // modify in place
+    let mut vec = Vec::from(list);
+
+    // We will first declare a pointer for the element we are
+    // attempting to move around
+    for i in 1usize..vec.len() {
+        // We will get the current element we are comparing against
+        let current = vec[i];
+        // We will also get the possible element to swap with, which is
+        // possibly the element before. We will swap our way back
+        let mut j:usize = i-1;
+
+        // as long as we have not hit the end & the prev number is bigger
+        // than the current one
+
+        // a quick note on the neq 0 business. [`usize`] in rust does not
+        // allow negative values, as, of course, it is unsigned.
+        // Therefore, if we need to swap into the beginning of the list,
+        // we have to have a special case for 0 comparisons.
+        while j > 0 && vec[j] > current {
+            // We swap the current element back, and move the next element
+            // into correct order
+
+            // Move the larger element up
+            vec[j+1] = vec[j];
+
+            // Move the smaller element down
+            vec[j] = current;
+
+            // Move the comparator pointer
+            j -= 1
+        }
+
+        // Special case for j=0, same swap
+        if j == 0 && vec[j] > current {
+            vec[j+1] = vec[j];
+            vec[j] = current;
+        }
+
+    }
+
+    // We finally return the now-sorted vector
+    return vec;
+}
+
+// Worse and best case insertion: do nothing + do everything
 fn best_case_insertion(size:usize) -> Vec<u32> {
     let vals: Vec<u32> = (0..size).map(|i| i as u32).collect();
     return vals;
@@ -81,24 +141,28 @@ fn worse_case_insertion(size:usize) -> Vec<u32> {
     return vals;
 }
 
-fn main() {
+// Define the mechanism to use in mainloop
+const MECHANISM:(&str, &dyn Fn(&[u32]) -> Vec<u32>) = ("insertion", &insertion_sort);
+// Define the max number of steps to have
+const STEPS:usize = 2;
 
-    for i in 0..100  {
-        let n = i*10;
+fn main() {
+    for i in 0..STEPS  {
+        let n = i*1000;
 
         let l0 = random_list(n);
         let t0 = Instant::now();
-        insertion_sort(&l0);
-        println!("\nSorted {} random digits via insertion sort. Elasped {:?}.", n, t0.elapsed());
+        MECHANISM.1(&l0);
+        println!("\nSorted {} random digits via {} sort. Elasped {:?}.", MECHANISM.0, n, t0.elapsed());
 
         let l1 = best_case_insertion(n);
         let t1 = Instant::now();
-        insertion_sort(&l1);
-        println!("Sorted {} best-case digits via insertion sort. Elasped {:?}.", n, t1.elapsed());
+        MECHANISM.1(&l1);
+        println!("Sorted {} best-case digits via {} sort. Elasped {:?}.", MECHANISM.0, n, t1.elapsed());
 
         let l2 = worse_case_insertion(n);
         let t2 = Instant::now();
-        insertion_sort(&l2);
-        println!("Sorted {} worse-case digits via insertion sort. Elasped {:?}.\n", n, t2.elapsed());
+        MECHANISM.1(&l2);
+        println!("Sorted {} worse-case digits via {} sort. Elasped {:?}.\n", MECHANISM.0, n, t2.elapsed());
     }
 }
